ec45e980a82efd70eb4f54660430a07f036bd894
Kumara Zaki Hibrizy <kumarazakihibrizy01@gmail.com>
2026-04-25 20:45:18 +0700

    Menambahkan fitur profile dan update konfigurasi

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -16,18 +16,23 @@
                 <span>Keranjang</span>
             </a>
         </li><!-- End Keranjang Nav --> 
-        <?php
-        if (session()->get('role') == 'admin') {
-        ?>     
+
+        <?php if (session()->get('role') == 'admin') { ?>     
         <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'produk') ? "" : "collapsed" ?>" href="produk">
                 <i class="bi bi-receipt"></i>
                 <span>Produk</span>
             </a>
         </li><!-- End Produk Nav -->
-        <?php 
-        } 
-        ?> 
+        <?php } ?> 
+
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li><!-- End Profile Nav -->
+
     </ul>
 
 </aside><!-- End Sidebar-->
